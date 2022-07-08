67460eaea1d32ce534d4539580c00ab5d2bd4bc4
s3833684 <s3833684@student.rmit.edu.au>
2022-07-08 18:14:23 +1000

    Bonus form for calculating the combo score of the combination.

--- src/components/BonusForm.jsx
+++ src/components/BonusForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
@@ -10,28 +10,61 @@ import Card_btn from './Card_btn';
 
 function BonusForm(props) {
   const [cardsArr, setCardsArr] = useState([]);
+  const [defaultCards, setDefaultCards] = useState([]);
 
-  var defaultCards = colors
-    .map((color) => {
-      return Object.keys(jobs).map((job) => {
-        return { color: color, job: job, clicked: false };
-      });
-    })
-    .reduce((prevArr, currArr) => prevArr.concat(currArr), []);
+  const [colorCombo, setColorCombo] = useState('');
+  const [colorScore, setColorScore] = useState('');
+  const [patternCombo, setPatternCombo] = useState('');
+  const [patternScore, setPatternScore] = useState('');
+
+  useEffect(() => {
+    var cards = colors
+      .map((color) => {
+        return Object.keys(jobs).map((job) => {
+          return { color: color, job: job, clicked: false };
+        });
+      })
+      .reduce((prevArr, currArr) => prevArr.concat(currArr), []);
+    setDefaultCards(cards);
+  }, []);
 
   const onClick = (card) => {
+    //if the card already in the array, remove it
+    const oldLength = cardsArr.length;
+    cardsArr.map((c) => console.log(c));
+    var newArr = cardsArr.filter(
+      (c) => c.job != card.job || c.color != card.color
+    );
+    setCardsArr(newArr);
+    if (newArr.length !== oldLength) {
+      card.clicked = !card.clicked;
+      setColorCombo('');
+      setColorScore('');
+      setPatternCombo('');
+      setPatternScore('');
+      return;
+    }
+    //if the card array is full, do nothing if it is not in the array
+    if (cardsArr.length == 3) return;
+
+    //normal situation, add card and if the array length reach 3, run the calculation
     cardsArr.push(card);
     setCardsArr(cardsArr);
     if (cardsArr.length === 3) {
-      console.log(calculateScore(cardsArr));
+      calculateScore(cardsArr);
     }
+
+    //update the default array to update the ui
     var i = defaultCards.findIndex(
       (c) => c.job == card.job && c.color == card.color
     );
-    console.log(i);
-    defaultCards[i].clicked = true;
-    // setCardsArr([]);
-    console.log(cardsArr.includes(card));
+    defaultCards.map((card, index) => {
+      if (index === i) {
+        card.clicked = !card.clicked;
+      }
+      return card;
+    });
+    setDefaultCards([...defaultCards]);
   };
 
   function calculateScore(arr) {
@@ -46,14 +79,17 @@ function BonusForm(props) {
     );
     //same color
     if (colorSet.size === 1) {
-      console.log('same color combo');
+      setColorCombo('same color');
+      setColorScore(3);
       return 3;
     }
     //arcane
     if (colorSet.size === 3) {
-      console.log('arcane');
+      setColorCombo('arcane');
+      setColorScore(2.5);
       return 2.5;
     }
+    setColorScore(1);
     return 1;
   }
 
@@ -71,12 +107,14 @@ function BonusForm(props) {
     });
     //if any one set has 3, it must be straight
     if (steelSet.size === 3 || magicSet.size === 3 || faithSet.size === 3) {
-      console.log('straight');
+      setPatternCombo('straight');
+      setPatternScore(6);
       return 6;
     }
     //if total size of the set is 1, must be triple
     if (steelSet.size + magicSet.size + faithSet.size === 1) {
-      console.log('triple');
+      setPatternCombo('triple');
+      setPatternScore(7.5);
       return 7.5;
     }
     //if two sets are empty, must be straight or flush, straight is returned above so must be flush
@@ -85,7 +123,8 @@ function BonusForm(props) {
       (!steelSet.size && !faithSet.size) ||
       (!magicSet.size && !faithSet.size)
     ) {
-      console.log('flush');
+      setPatternCombo('flush');
+      setPatternScore(3);
       return 3;
     }
     //if any one of the set is empty, and the total is 2, must be double
@@ -93,22 +132,30 @@ function BonusForm(props) {
       (!steelSet.size || !magicSet.size || !faithSet.size) &&
       steelSet.size + magicSet.size + faithSet.size === 2
     ) {
-      console.log('double');
+      setPatternCombo('double');
+      setPatternScore(2);
       return 2;
     }
+    setPatternScore(1);
     return 1;
   }
 
   return (
     <div>
-      <h1>Color combo:{}</h1>
+      <h1>Combo score: {colorScore * patternScore}</h1>
+      <h1>
+        Color combo: {colorCombo} {colorScore}
+      </h1>
+      <h1>
+        Pattern combo: {patternCombo} {patternScore}
+      </h1>
       <Box sx={{ width: '100%', flexgrow: 1 }}>
         <Grid container spacing={2}>
           {defaultCards.map((card) => {
             return (
               <Grid item xs={1.33}>
                 <Card_btn
-                  variant={cardsArr.includes(card) ? 'contained' : 'outlined'}
+                  variant={card.clicked ? 'outlined' : 'contained'}
                   color={card.color}
                   job={jobs[card.job]}
                   onClick={() => onClick(card)}
